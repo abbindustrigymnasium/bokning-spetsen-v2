f09660e2ec965d8ea6d11ef579407543b7bca1f6
hg25Elllof <elliot.lofstaf@hitachigymnasiet.se>
2026-08-24 17:53:31 +0200

    basic calendar and booking page (non working).

---
+++ src/lib/calendar.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	let { date }: { date: Date } = $props();
+</script>
+
+<div class="h-100 w-3/5">
+	<div class="h-1/4 w-full bg-amber-300"></div>
+	<div class="h-1/4 w-full bg-green-300"></div>
+	<div class="h-1/4 w-full bg-blue-300"></div>
+	<div class="h-1/4 w-full bg-red-300"></div>
+</div>
---
+++ src/routes/book/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+	import Calendar from '$lib/calendar.svelte';
+</script>
+
+<div class="text-center">
+	<p class="text-4xl font-bold">Bokaren</p>
+	Vänligen välj ett datum och en tid.
+</div>
+
+<input type="date" />
+
+<Calendar />
